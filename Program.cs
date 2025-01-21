43b49845b21975e979238216b1c7d5fd7777af2c
Eden.Ye <Eden.Ye@wisetechglobal.com>
2025-01-21 13:13:27 +0800

    Update result

--- Program.cs
+++ Program.cs
@@ -116,13 +116,20 @@ namespace StringBenchmark
             var summary = BenchmarkRunner.Run<StringConcatBenchmarks>();
             Console.WriteLine(summary);
         }
-        // | Method              | Mean     | Error    | StdDev   | Median    |
-        // |-------------------- |---------:|---------:|---------:|----------:|
-        // | ConcatOperator      | 11.50 ns | 0.485 ns | 1.430 ns | 11.611 ns |
-        // | InterpolatedString  | 11.08 ns | 0.400 ns | 1.160 ns | 11.406 ns |
-        // | SpanConcat          | 10.08 ns | 0.395 ns | 1.165 ns |  9.777 ns |
-        // | StringConcat        | 11.01 ns | 0.369 ns | 1.088 ns | 10.738 ns |
-        // | Join                | 37.79 ns | 1.322 ns | 3.878 ns | 38.374 ns |
-        // | StringBuilderAppend | 15.83 ns | 0.703 ns | 2.062 ns | 15.046 ns |
+        // BenchmarkDotNet v0.14.0, Windows 11 (10.0.22631.4751/23H2/2023Update/SunValley3)
+        // Intel Core Ultra 9 185H, 1 CPU, 22 logical and 16 physical cores
+        // .NET SDK 8.0.404
+        //   [Host]     : .NET 8.0.12 (8.0.1224.60305), X64 RyuJIT AVX2
+        //   DefaultJob : .NET 8.0.12 (8.0.1224.60305), X64 RyuJIT AVX2
+
+
+        // | Method              | Mean     | Error    | StdDev   | Median   | Gen0   | Allocated |
+        // |-------------------- |---------:|---------:|---------:|---------:|-------:|----------:|
+        // | ConcatOperator      | 10.72 ns | 0.331 ns | 0.945 ns | 10.74 ns | 0.0083 |     104 B |
+        // | InterpolatedString  | 11.19 ns | 0.280 ns | 0.812 ns | 11.15 ns | 0.0083 |     104 B |
+        // | SpanConcat          | 10.39 ns | 0.375 ns | 1.107 ns | 10.38 ns | 0.0045 |      56 B |
+        // | StringConcat        | 11.39 ns | 0.278 ns | 0.808 ns | 11.43 ns | 0.0083 |     104 B |
+        // | Join                | 36.19 ns | 0.830 ns | 2.448 ns | 36.52 ns | 0.0114 |     144 B |
+        // | StringBuilderAppend | 16.01 ns | 0.371 ns | 0.957 ns | 15.69 ns | 0.0127 |     160 B |
     }
 }
